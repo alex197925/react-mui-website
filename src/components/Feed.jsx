f3fb9435f2e1b816dd381d3c5f65191c496b6edf
Tolok Oleksandr <tolokoleksandr@Toloks-MacBook-Pro.local>
2023-10-30 16:51:43 +0100

    Created Posts

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,59 +1,16 @@
 /** @format */
-import { Favorite, MoreVert, Share } from "@mui/icons-material";
-
-import {
-  Avatar,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  IconButton,
-  Typography,
-} from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
+import Post from "./Post";
 
 const Feed = () => {
   return (
-    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
-      <Card>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: "red" }} aria-label='recipe'>
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label='settings'>
-              <MoreVert />
-            </IconButton>
-          }
-          title='Shrimp and Chorizo Paella'
-          subheader='September 14, 2016'
-        />
-        <CardMedia
-          component='img'
-          height='194'
-          image='https://plus.unsplash.com/premium_photo-1697968234949-854d20ef0e7a?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-          alt='Paella dish'
-        />
-        <CardContent>
-          <Typography variant='body2' color='text.secondary'>
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label='add to favorites'>
-            <Favorite />
-          </IconButton>
-          <IconButton aria-label='share'>
-            <Share />
-          </IconButton>
-        </CardActions>
-      </Card>
+    <Box flex={4} p={2}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </Box>
   );
 };
