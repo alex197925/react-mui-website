06e101a92b244273c576a7310a2ec1e709fd3422
Tolok Oleksandr <tolokoleksandr@Toloks-MacBook-Pro.local>
2023-10-30 16:52:58 +0100

    Created Post component, transfer feeds to Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,64 @@
+/** @format */
+
+import { Favorite, FavoriteBorder, MoreVert, Share } from "@mui/icons-material";
+
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Checkbox,
+  IconButton,
+  Typography,
+} from "@mui/material";
+
+const Post = () => {
+  return (
+    <div>
+      <Card sx={{ margin: 5 }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: "red" }} aria-label='recipe'>
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label='settings'>
+              <MoreVert />
+            </IconButton>
+          }
+          title='John Smith'
+          subheader='September 14, 2023'
+        />
+        <CardMedia
+          component='img'
+          height='20%'
+          image='https://plus.unsplash.com/premium_photo-1697968234949-854d20ef0e7a?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt='Paella dish'
+        />
+        <CardContent>
+          <Typography variant='body2' color='text.secondary'>
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label='add to favorites'>
+            <Checkbox
+              icon={<FavoriteBorder />}
+              checkedIcon={<Favorite sx={{ color: "red" }} />}
+            />
+          </IconButton>
+          <IconButton aria-label='share'>
+            <Share />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
+
+export default Post;
